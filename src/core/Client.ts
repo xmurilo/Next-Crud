9257582d1303d6d3395642b02374f78f56268a3c
Murilo <dixmurilox@gmail.com>
2024-07-21 22:18:55 -0300

    feat: Add Client class for managing client data

---
+++ src/core/Client.ts
@@ -0,0 +1,27 @@
+export default class Client {
+  private id: string;
+  private name: string;
+  private age: number;
+
+  constructor(name: string, age: number, id: string | null = null) {
+    this.name = name;
+    this.age = age;
+    this.id = id || "default-id"; // Provide a default string value if id is null
+  }
+
+  static empty() {
+    return new Client("", 0);
+  }
+
+  get getId() {
+    return this.id;
+  }
+
+  get getName() {
+    return this.name;
+  }
+
+  get getAge() {
+    return this.age;
+  }
+}
