7bfc28684b6dfaff930849ca976c64259cb85878
Murilo <dixmurilox@gmail.com>
2024-07-21 21:20:05 -0300

    refactor: Update home page layout and structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600 via-yellow-500 to-blue-500 ">
-      <span className="text-4xl">text</span>
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white ">
+      <Layout title="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   );
 }
