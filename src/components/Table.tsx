4cda7d13159fa1d1f00a5d133c00082b40b1f601
Murilo <dixmurilox@gmail.com>
2024-07-22 17:42:10 -0300

    refactor: Add actions column to Table component for selected and deleted customers

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,31 +1,65 @@
 import Client from "../core/Client";
-
+import { EditionIcon } from "./Icons";
+import { TrashIcon } from "./Icons";
 interface TableProps {
   clients: Client[];
+  selectedCustomer?: (client: Client) => void;
+  deletedCustomer?: (client: Client) => void;
 }
 
 export default function Table(props: TableProps) {
+  const showActions = props.selectedCustomer && props.deletedCustomer;
+
   function renderHeaderTable(): JSX.Element {
     return (
       <tr>
         <th className="text-left p-4 ">Código</th>
         <th className="text-left p-4 ">Nome</th>
         <th className="text-left p-4 ">Idade</th>
+        {showActions ? <th className="text-left p-4 ">Ações</th> : false}
       </tr>
     );
   }
 
-  function renderClients(): React.ReactNode {
+  function renderData(): React.ReactNode {
     return props.clients?.map((client, i) => {
       return (
-        <tr key={client.getId} 
-          className={`${i % 2 == 0 ? "bg-purple-200" : "bg-purple-100"}`}>
+        <tr key={client.getId} className={`${i % 2 == 0 ? "bg-purple-200" : "bg-purple-100"}`}>
           <td className="text-left p-4 ">{client.getId}</td>
           <td className="text-left p-4 ">{client.getName}</td>
           <td className="text-left p-4 ">{client.getAge}</td>
+          {showActions ? renderActions(client) : false}
         </tr>
       );
     });
+  }
+
+  function renderActions(client: Client) {
+    return (
+      <td className="flex justify-center">
+        {props.selectedCustomer ? (
+          <button
+            className={`flex justify-center items-center text-green-600 rounded-full hover:bg-purple-50 p-2 m-1`}
+            onClick={() => props.selectedCustomer?.(client)}
+          >
+            {EditionIcon}
+          </button>
+        ) : (
+          false
+        )}
+
+        {props.deletedCustomer ? (
+          <button
+            className={`flex justify-center items-center text-red-500 rounded-full hover:bg-purple-50 p-2 m-1`}
+            onClick={() => props.deletedCustomer?.(client)}
+          >
+            {TrashIcon}
+          </button>
+        ) : (
+          false
+        )}
+      </td>
+    );
   }
 
   return (
@@ -33,7 +67,7 @@ export default function Table(props: TableProps) {
       <thead className={` text-gray-100 bg-gradient-to-r from-purple-500 to-purple-800  `}>
         {renderHeaderTable()}
       </thead>
-      <tbody>{renderClients()}</tbody>
+      <tbody>{renderData()}</tbody>
     </table>
   );
 }
