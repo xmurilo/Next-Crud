5745509c802144117c247b673c0b17720f563e74
Murilo <dixmurilox@gmail.com>
2024-07-21 21:02:36 -0300

    chore: Add Next.js configuration files and global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600 via-yellow-500 to-blue-500 ">
+      <span className="text-4xl">text</span>
+    </div>
+  );
+}
