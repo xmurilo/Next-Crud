903a5b0cc91829bddbef85268de6b719da5d9efa
Murilo <dixmurilox@gmail.com>
2024-07-22 00:33:26 -0300

    feat: Update home page to display client data in a table format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 export default function Home() {
+  const clients = [
+    new Client("Ana", 34, "1"),
+    new Client("Bia", 45, "2"),
+    new Client("Carlos", 23, "3"),
+    new Client("Daniel", 54, "4"),
+  ];
+
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white ">
       <Layout title="Cadastro Simples">
-        <span>Conteudo</span>
+        <Table clients={clients}></Table>
       </Layout>
     </div>
   );
