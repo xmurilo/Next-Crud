325eed287401fac327fa7720ea631961a14f52be
Murilo <dixmurilox@gmail.com>
2024-07-22 23:37:28 -0300

    refactor: Update Home page to use Customer instead of Client and add form functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,31 @@ import Button from "../components/Button";
 import Form from "../components/Form";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
-import Client from "../core/Client";
+import Customer from "../core/Customer";
 
 export default function Home() {
-  const clients = [
-    new Client("Ana", 34, "1"),
-    new Client("Bia", 45, "2"),
-    new Client("Carlos", 23, "3"),
-    new Client("Daniel", 54, "4"),
+  const [visible, setVisible] = useState<"table" | "form">("table");
+  const [customer, setCustomer] = useState<Customer>(Customer.empty());
+
+  const customers = [
+    new Customer("Ana", 34, "1"),
+    new Customer("Bia", 45, "2"),
+    new Customer("Carlos", 23, "3"),
+    new Customer("Daniel", 54, "4"),
   ];
 
-  function selectedCustomer(client: Client) {
-    console.log(client.getName);
-  }
-  function deletedCustomer(client: Client) {
-    console.log("Excluindo..." + client.getName);
+  function selectedCustomer(customer: Customer) {
+    setCustomer(customer);
   }
 
-  function saveCustomer(client: Client) {
-    console.log(client);
+  function deletedCustomer(customer: Customer) {
+    console.log("Excluindo..." + customer.getName);
   }
 
-  const [visible, setVisible] = useState<"table" | "form">("table");
+  function saveCustomer(customer: Customer) {
+    console.log(customer);
+    setVisible("table");
+  }
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white ">
@@ -38,14 +41,14 @@ export default function Home() {
             </div>
 
             <Table
-              clients={clients}
+              customers={customers}
               selectedCustomer={selectedCustomer}
               deletedCustomer={deletedCustomer}
             ></Table>
           </>
         ) : (
           <Form
-            client={clients[0]}
+            customer={customers[0]}
             changeCustomer={saveCustomer}
             canceled={() => setVisible("table")}
           />
