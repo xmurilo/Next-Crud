5af2a2471940055b97c684f844eef84b959dafdf
Murilo <dixmurilox@gmail.com>
2024-07-23 20:55:09 -0300

    refactor: Update Home component to use useCustomer hook for managing customer data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,30 @@
+import { useEffect } from "react";
 import Button from "../components/Button";
 import Form from "../components/Form";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
 import useCustomer from "../hooks/useCustomers";
-import useTableOrForm from "../hooks/useTableOrForm";
 
 export default function Home() {
-  const { customer, customers, newCustomer, saveCustomer, deletedCustomer, selectedCustomer } =
-    useCustomer();
+  const {
+    customer,
+    customers,
+    visibleTable,
+    showTable,
+    newCustomer,
+    saveCustomer,
+    deletedCustomer,
+    selectedCustomer,
+  } = useCustomer();
 
-  const { tableVisible, showTable } = useTableOrForm();
+  useEffect(() => {
+    console.log(visibleTable);
+  }, []);
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white ">
       <Layout title="Cadastro Simples">
-        {tableVisible ? (
+        {visibleTable ? (
           <>
             <div className="flex justify-end">
               <Button className="mb-4" onClick={newCustomer}>
