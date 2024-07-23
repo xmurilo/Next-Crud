6fafae1d51c991bae8fcd6fdc2b12f022c6703b3
Murilo <dixmurilox@gmail.com>
2024-07-23 18:36:44 -0300

    refactor: Update useCustomer hook to manage customer data and form visibility

---
+++ src/hooks/useCustomers.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import Customer from "../core/Customer";
+import RepositoryCustomer from "../core/RepositoryCustomer";
+import CollectionCustomer from "../backend/db/CollectionCustomer";
+import useTableOrForm from "./useTableOrForm";
+
+export default function useCustomer() {
+  const repo: RepositoryCustomer = new CollectionCustomer();
+
+  const {showForm, showTable } = useTableOrForm();
+
+  const [customer, setCustomer] = useState<Customer>(Customer.empty());
+  const [customers, setCustomers] = useState<Customer[]>([]);
+
+  useEffect(getAllCustomers, []);
+
+  function getAllCustomers() {
+    repo.getAll().then(customers => {
+      setCustomers(customers);
+      showTable()
+    });
+  }
+
+  function selectedCustomer(customer: Customer) {
+    setCustomer(customer);
+    showForm()
+  }
+
+  async function deletedCustomer(customer: Customer) {
+    await repo.delete(customer);
+    getAllCustomers();
+  }
+
+  function newCustomer() {
+    setCustomer(Customer.empty());
+    showForm()
+  }
+
+  async function saveCustomer(customer: Customer) {
+    await repo.save(customer);
+    getAllCustomers();
+  }
+
+  return {
+    customer,
+    customers,
+    newCustomer,
+    saveCustomer,
+    deletedCustomer,
+    selectedCustomer,
+    getAllCustomers,
+  };
+}
