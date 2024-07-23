cceb41b51724b7afc56ae7c88552c47cf5ef3ca1
Murilo <dixmurilox@gmail.com>
2024-07-22 23:37:04 -0300

    refactor: Update Form component to use Customer instead of Client

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 import Input from "./Input";
-import Client from "../core/Client";
+import Customer from "../core/Customer";
 import Button from "./Button";
 
 interface FormProps {
-  client: Client;
-  changeCustomer?: (client: Client) => void;
+  customer: Customer;
+  changeCustomer?: (customer: Customer) => void;
   canceled?: () => void;
 }
 
 export default function Form(props: FormProps) {
-  const id = props.client?.getId;
+  const id = props.customer?.getId;
 
-  const [name, setName] = useState(props.client?.getName ?? "");
-  const [age, setAge] = useState(props.client?.getAge ?? 0);
+  const [name, setName] = useState(props.customer?.getName ?? "");
+  const [age, setAge] = useState(props.customer?.getAge ?? 0);
 
   return (
     <div>
@@ -26,7 +26,7 @@ export default function Form(props: FormProps) {
         <Button
           color="blue"
           className="mr-2"
-          onClick={() => props.changeCustomer?.(new Client(name, +age, id))}
+          onClick={() => props.changeCustomer?.(new Customer(name, +age, id))}
         >
           {id ? "Alterar" : "Salvar"}
         </Button>
