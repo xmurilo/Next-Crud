63b46a60cac801bdff65dd03f750797133adf5cc
Murilo <dixmurilox@gmail.com>
2024-07-22 22:52:31 -0300

    feat: Add onClick event handler to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,14 @@ interface ButtonProps {
   children: any;
   color?: "green" | "blue" | "gray";
   className?: string;
+  onClick?: () => void
 }
+
 export default function Button(props: ButtonProps) {
   const color = props.color ?? "blue"
   return (
     <button
+    onClick={props.onClick}
       className={`
     bg-gradient-to-r from-${color}-400 to-${color}-700
     text-white px-4 py-2 rounded-md
