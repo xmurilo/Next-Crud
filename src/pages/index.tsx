67a2121271657418c5e604a167b2dbd681e55a20
Murilo <dixmurilox@gmail.com>
2024-07-23 18:29:20 -0300

    refactor: Update Home component to use useCustomer hook for managing customer data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,20 @@
-import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import Form from "../components/Form";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
-import Customer from "../core/Customer";
-import RepositoryCustomer from "../core/RepositoryCustomer";
-import CollectionCustomer from "../backend/db/CollectionCustomer";
+import useCustomer from "../hooks/useCustomers";
+import useTableOrForm from "../hooks/useTableOrForm";
 
 export default function Home() {
-  const repo: RepositoryCustomer = new CollectionCustomer();
+  const { customer, customers, newCustomer, saveCustomer, deletedCustomer, selectedCustomer } =
+    useCustomer();
 
-  const [customer, setCustomer] = useState<Customer>(Customer.empty());
-  const [customers, setCustomers] = useState<Customer[]>([]);
-  const [visible, setVisible] = useState<"table" | "form">("table");
-
-  useEffect(getAllCustomers, []);
-
-  function getAllCustomers() {
-    repo.getAll().then(customers => {
-      setCustomers(customers);
-      setVisible("table");
-    });
-  }
-
-  function selectedCustomer(customer: Customer) {
-    setCustomer(customer);
-    setVisible("form");
-  }
-
-  async function deletedCustomer(customer: Customer) {
-    await repo.delete(customer);
-    getAllCustomers();
-  }
-
-  function newCustomer() {
-    setCustomer(Customer.empty());
-    setVisible("form");
-  }
-
-  async function saveCustomer(customer: Customer) {
-    await repo.save(customer);
-    getAllCustomers();
-  }
+  const { tableVisible, showTable } = useTableOrForm();
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white ">
       <Layout title="Cadastro Simples">
-        {visible == "table" ? (
+        {tableVisible ? (
           <>
             <div className="flex justify-end">
               <Button className="mb-4" onClick={newCustomer}>
@@ -61,11 +29,7 @@ export default function Home() {
             ></Table>
           </>
         ) : (
-          <Form
-            customer={customer}
-            changeCustomer={saveCustomer}
-            canceled={() => setVisible("table")}
-          />
+          <Form customer={customer} changeCustomer={saveCustomer} canceled={showTable} />
         )}
       </Layout>
     </div>
